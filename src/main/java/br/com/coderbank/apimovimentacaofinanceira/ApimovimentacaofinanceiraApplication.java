6a4a30880d1b40821380134b110c638e32538642
Luan Diniz <luancsdiniz@outlook.com>
2026-03-23 17:23:29 -0300

    first commit

---
+++ src/main/java/br/com/coderbank/apimovimentacaofinanceira/ApimovimentacaofinanceiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.coderbank.apimovimentacaofinanceira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimovimentacaofinanceiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimovimentacaofinanceiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/coderbank/apimovimentacaofinanceira/ApimovimentacaofinanceiraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coderbank.apimovimentacaofinanceira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimovimentacaofinanceiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
