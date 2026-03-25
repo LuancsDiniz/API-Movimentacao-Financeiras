7f49eea172043207ffc6e98f7cfe9d28bf218e92
Luan Diniz <luancsdiniz@outlook.com>
2026-03-25 10:53:45 -0300

    removido OpenFeign, cliente externo e configurações relacionadas

--- src/main/java/br/com/coderbank/apimovimentacaofinanceira/feign/ClienteClient.java
+++
@@ -1,7 +0,0 @@
-package br.com.coderbank.apimovimentacaofinanceira.feign;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "clienteClient", url = "${cliente.api.url}")
-public interface ClienteClient {
-}
